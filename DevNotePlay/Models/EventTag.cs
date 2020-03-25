dc8feea29b4416bd21b9e01849716e1a1de54199
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-03-25 14:14:13 +0800

    add EventTag service

---
+++ DevNotePlay/Models/EventTag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player.Models
+{
+    public class EventTag
+    {
+        public int Id { get; set; }
+        public string Domain { get; set; }
+        public string Department { get; set; }
+        public string Tag { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SourcePath { get; set; }
+        public bool InActive { get; set; }
+        public int VersionNo { get; set; }
+        public string FileName { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
---
+++ DevNotePlay/Services/EventTagService.cs
@@ -0,0 +1,36 @@
+﻿using LogApplication.Common.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using Player.Models;
+
+namespace Player.Services
+{
+    public class EventTagService
+    {
+        private readonly string _url;
+
+        public EventTagService()
+        {
+            ConfigManager config = new ConfigManager();
+
+            _url = config.GetValue("DevNoteFrontUrl_dev") + "/api/events";
+        }
+
+        public List<EventTag> GetEventTagLibraryFromServer()
+        {
+            try
+            {
+                var result = _url.GetJsonFromUrl().FromJson<List<EventTag>>();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new List<EventTag>();
+            }
+        }
+    }
+}
