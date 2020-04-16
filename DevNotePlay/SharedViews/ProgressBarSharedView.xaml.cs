a03f287fd6af48c081f023127120a5ef63ba8fa0
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-04-16 13:55:46 +0800

    add Progress Bar

---
+++ DevNotePlay/SharedViews/ProgressBarSharedView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Player.SharedViews
+{
+    /// <summary>
+    /// Interaction logic for ProgressBarSharedView.xaml
+    /// </summary>
+    public partial class ProgressBarSharedView : Window
+    {
+        public ProgressBarSharedView(string caption)
+        {
+            InitializeComponent();
+            StatusLabel.Content = caption;
+        }
+    }
+}
