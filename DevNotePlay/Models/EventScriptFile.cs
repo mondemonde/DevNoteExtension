19833eb12486ee99fe3f80c6f123f995a6748855
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-04-20 12:36:54 +0800

    add extra properties for DownloadScriptFile

--- DevNotePlay/Models/EventScriptFile.cs
+++ DevNotePlay/Models/EventScriptFile.cs
@@ -1,9 +1,25 @@
-﻿namespace Player.Models
+﻿using System.IO;
+
+namespace Player.Models
 {
     public class EventScriptFile
     {
         public string Name { get; set; }
         public string SourcePath { get; set; }
         public string Content { get; set; }
+        public string FileNameWithExtension
+        {
+            get
+            {
+                return Path.GetFileName(SourcePath);
+            }
+        }
+        public string ParentFolder
+        {
+            get
+            {
+                return Path.GetFileName(Path.GetDirectoryName(SourcePath));
+            }
+        }
     }
 }
