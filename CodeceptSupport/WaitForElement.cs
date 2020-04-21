05cc6c97b4d01359ccce1da9c0ba3c65c1519743
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-04-21 14:07:36 +0800

    add WaitForElement keyword

---
+++ CodeceptSupport/WaitForElement.cs
@@ -0,0 +1,36 @@
+﻿using PuppetSupportLib;
+using PuppetSupportLib.Katalon;
+using PuppetSupportLib.WebAction;
+using System;
+
+namespace CodeceptSupport
+{
+    public class WaitForElement : BaseAction
+    {
+        public WaitForElement(TestCaseSelenese katalonxml) : base(katalonxml)
+        {
+
+        }
+
+        public override TestCaseSelenese Map(object customAction)
+        {
+            //throw new NotImplementedException();
+            var act = (TestCaseSelenese)customAction;
+            //do convettion here..
+            //..
+            //.
+            return act;
+        }
+
+        public override string Script(IInterpreter interpreter)
+        {
+            string target = MyAction.target;
+            string value = MyAction.value;
+
+            var script = string.Format("waitForElement(\"{0}\", {1});", target, value);
+            script = script + Environment.NewLine;
+
+            return script;
+        }
+    }
+}
