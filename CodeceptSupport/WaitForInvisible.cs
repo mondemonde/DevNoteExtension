9c81529027e3adbc07ca6332fa49e9536667acad
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-04-22 09:50:18 +0800

    add waitForElementNotVisible keyword

---
+++ CodeceptSupport/WaitForInvisible.cs
@@ -0,0 +1,40 @@
+﻿using PuppetSupportLib;
+using PuppetSupportLib.Katalon;
+using PuppetSupportLib.WebAction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeceptSupport
+{
+    public class WaitForInvisible : BaseAction
+    {
+        public WaitForInvisible(TestCaseSelenese katalonxml) : base(katalonxml)
+        {
+
+        }
+
+        public override TestCaseSelenese Map(object customAction)
+        {
+            //throw new NotImplementedException();
+            var act = (TestCaseSelenese)customAction;
+            //do convettion here..
+            //..
+            //.
+            return act;
+        }
+
+        public override string Script(IInterpreter interpreter)
+        {
+            string target = MyAction.target;
+            string value = MyAction.value;
+
+            var script = string.Format("waitForInvisible(\"{0}\", {1});", target, value);
+            script = script + Environment.NewLine;
+
+            return script;
+        }
+    }
+}
