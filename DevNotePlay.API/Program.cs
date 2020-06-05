7a9fc59d8d196ae7e5fd2e6a604589b0df215033
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-06-05 19:09:34 +0800

    Remove unnecessary code

--- DevNotePlay.API/Program.cs
+++ DevNotePlay.API/Program.cs
@@ -12,14 +12,7 @@ namespace DevNotePlay.API
             // Start OWIN host 
             using (WebApp.Start<Startup>(url: baseAddress))
             {
-                //Create HttpClient and make a request to api/values 
-                //HttpClient client = new HttpClient();
-
-                //var response = client.GetAsync(baseAddress + "api/").Result;
-
-                //Console.WriteLine(response);
-                //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-                Console.WriteLine("API app started.");
+                Console.WriteLine("DevNotePlay API running.");
                 Console.WriteLine(String.Format("Listening on: {0}", baseAddress));
                 Console.ReadLine();
             }
