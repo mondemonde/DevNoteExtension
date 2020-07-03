7c5c1da1777f11eeddf0e3d9ff05f0323df938d0
aaroncustodio <custodio.aaron.1197@gmail.com>
2020-07-03 12:40:45 +0800

    Add WindowController and WindowHelper

---
+++ DevNotePlay/Controllers/WindowController.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using Player.Extensions;
+using Player.Views;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Windows;
+
+namespace Player.Controllers
+{
+    [RoutePrefix("api/window")]
+    public class WindowController : ApiController
+    {
+        [Route("create-event")]
+        [HttpPost]
+        public HttpResponseMessage OpenCreateEventWindow([FromBody]dynamic payload)
+        {
+            try
+            {
+                string latestXML = FileEndPointManager.DefaultLatestXMLFile;
+                string latestHtml = FileEndPointManager.DefaultLatestHtmlFile;
+                string xmlContent = payload["xml"];
+                string htmlContent = payload["html"];
+
+                if (File.Exists(latestXML))
+                    File.Delete(latestXML);
+                if (File.Exists(latestHtml))
+                    File.Delete(latestHtml);
+
+                File.WriteAllText(latestXML, xmlContent);
+                File.WriteAllText(latestHtml, htmlContent);
+
+                Application.Current.Dispatcher.Invoke((Action)delegate {
+                    WindowHelper.OpenWindow<AddEventWindow>();
+                });
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception exc)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, exc.Message);
+            }
+        }
+    }
+}
---
+++ DevNotePlay/Extensions/WindowHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace Player.Extensions
+{
+    public static class WindowHelper
+    {
+        public static void OpenWindow<T>() where T : Window
+        {
+            var windows = Application.Current.Windows.Cast<Window>();
+
+            var any = windows.Any(s => s is T);
+
+            if (any)
+            {
+                var win = windows.Where(s => s is T).ToList()[0];
+
+                if (win.WindowState == WindowState.Minimized)
+                    win.WindowState = WindowState.Normal;
+
+                win.Focus();
+            }
+            else
+            {
+                object[] parameters = new object[1]
+                {
+                    null
+                };
+                var win = (Window)Activator.CreateInstance(typeof(T), parameters);
+                win.Show();
+            }
+        }
+    }
+}
